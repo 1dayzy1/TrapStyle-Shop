bba0861cbe55047c239509df977f1fee16150add
1dayzy1 <dayzy173@gmail.com>
2025-05-04 15:46:52 +0300

    edit

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-
+// vue.config.js
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Vue-shop/'
-    : '/'
-}
+  publicPath: '/', // или './', в зависимости от вашей конфигурации
+  // ... другие настройки
+};
