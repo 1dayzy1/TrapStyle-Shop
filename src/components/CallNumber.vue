c508f88a6e4f527757ecf1fe9d56007c3e8e1c45
1dayzy1 <dayzy173@gmail.com>
2025-05-11 21:13:06 +0300

    init

--- src/components/CallNumber.vue
+++ src/components/CallNumber.vue
@@ -3,7 +3,7 @@
     <div class="container-Call">
 
         <p class="sale">По всем вопросам</p>
-        <p class="number-sale">+7 495 123-45-67</p>
+        <p class="number-sale">+7 979 027-57-12</p>
 
 
     </div>
--- src/components/DrawerBlock.vue
+++ src/components/DrawerBlock.vue
@@ -70,7 +70,7 @@
     <b>Сумма: {{ sum }} рублей</b>
 
     
-    <div class=" w-50 h-10 flex justify-around items-center cursor-pointer  bg-lime-300  rounded-md mt-6 hover:bg-lime-400 transition ">Оформить заказ <svg xmlns="http://www.w3.org/2000/svg"  width="20" height="20" fill="currentColor" class="" viewBox="0 0 16 16">
+    <div class="hidden w-50 h-10 flex justify-around items-center cursor-pointer  bg-lime-300  rounded-md mt-6 hover:bg-lime-400 transition ">Оформить заказ <svg xmlns="http://www.w3.org/2000/svg"  width="20" height="20" fill="currentColor" class="" viewBox="0 0 16 16">
       <path fill-rule="evenodd"
         d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
     </svg></div>
